b955956d9fb0a749c9a8dd949c9d2470ef379000
Seop(Home Mac) <edenhazard5870@gmail.com>
2020-10-19 16:58:37 +0900

    jackson Deserialize refactoring

--- src/main/java/just/hazard/mockserver/entity/Todo.java
+++ src/main/java/just/hazard/mockserver/entity/Todo.java
@@ -2,20 +2,22 @@ package just.hazard.mockserver.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 @Builder(builderClassName = "TodoBuilder", toBuilder = true)
 @JsonDeserialize(builder = Todo.TodoBuilder.class)
 public class Todo {
 
-    @JsonProperty("id")
-    private Long id;
-    @JsonProperty("title")
-    private String title;
-    @JsonProperty("description")
-    private String description;
+    private final Long id;
+
+    private final String title;
+
+    private final String description;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class TodoBuilder {}
 }
